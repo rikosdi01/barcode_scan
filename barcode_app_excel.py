1af08c0d2d84f95a76c13e86f52f99f7f7683f9f
RIKO SDI01 <rikoparts.stokbd@gmail.com>
2026-05-02 15:20:59 +0700

    first commit

---
+++ barcode_app_excel.py
@@ -0,0 +1,16 @@
+from pyzbar import pyzbar
+import cv2
+
+def decode(image):
+    # decodes all barcodes from an image
+    decoded_objects = pyzbar.decode(image)
+    for obj in decoded_objects:
+        # draw the barcode
+        print("detected barcode:", obj)
+        image = draw_barcode(obj, image)
+        # print barcode type & data
+        print("Type:", obj.type)
+        print("Data:", obj.data)
+        print()
+
+    return image
